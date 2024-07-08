d022e5ddf0127be2ab0cf9d64ef07ab89fc93d7e
Noa Altshuler <noaalts@gmail.com>
2024-07-08 14:04:23 +0300

    no message

--- Ex05/MenuItem.cs
+++ Ex05/MenuItem.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 namespace Ex05
 {
-	public class MenuItem
+	public interface MenuItem
 	{
-		public MenuItem()
+		void action();
+	}
+
+	public class Menu
+	{
+		string title;
+		int NumberOfItems = 0;
+		List<MenuItem> Items;
+		bool exitOrBackFlag;
+	}
+
+	public class MainMenu: Menu
+	{
+
+	}
+
+	public class tatMenu : Menu, MenuItem
+	{
+		void MenuItem.action()
 		{
+
 		}
 	}
+	
 }
 
